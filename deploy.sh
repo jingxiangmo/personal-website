8db2513dbb0a909f2573ea2abc3eb857ed8ea0a9
Jingxiang Mo <65676392+Xetile@users.noreply.github.com>
2021-12-09 18:41:28 -0500

    Improved favcon and description

--- deploy.sh
+++ deploy.sh
@@ -3,14 +3,32 @@
 # if there's an error, exit the script
 set -e
 
+echo "============================================================"
+echo "Deploying the project..."
+echo "============================================================"
+echo 
+# deploy script for the webapp
+npm run deploy
+
+echo 
+echo "=========================================================="
+echo "Committing and pushing to github..."
+echo "=========================================================="
+echo
+echo "Commit messgage: $1"
+echo 
+
 # commit and push to github take arugment as the commit message
 git add .
 git commit -m "$1"
 git push
 
-# deploy script for the webapp
-echo "Deploying the project..."
-npm run deploy
+
+echo 
+echo "=========================================================="
+echo "Deployment complete."
+echo "=========================================================="
+echo 
 
 # website url
 echo "Website url: https://www.jingxiangmo.com"
