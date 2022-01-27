2d810fa8e72107bca53e94ff583df64e313fc3a2
Jingxiang Mo <jingxiangmo@gmail.com>
2022-01-27 15:27:17 -0500

    better script

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,12 @@
 # if there's an error, exit the script
 set -e
 
+# If $1 is empty, return an error message
+if [ -z "$1" ]; then
+  echo "ERROR: Usage: deploy.sh <commit message>"
+  exit 1
+fi
+
 
 echo "============================================================"
 echo "Deploying the project..."
@@ -17,14 +23,13 @@ echo "=========================================================="
 echo "Committing and pushing to github..."
 echo "=========================================================="
 echo
-echo "Commit messgage: $1"
+echo "Commit message> $1"
 echo 
 
-# commit and push to github take arugment as the commit message
+# commit and push to github take argument as the commit message
 git add .
 git commit -m "$1"
 git push
-
 
 echo 
 echo "=========================================================="
